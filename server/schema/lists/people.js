e6d1f3929079022659db5fb4aa73cba610d8cc1d
Davide Fiorello <davide@codeflyer.com>
2016-10-13 11:46:57 +0200

    added parameter to the people list

--- server/schema/lists/people.js
+++ server/schema/lists/people.js
@@ -1,6 +1,7 @@
 
 import {
-    GraphQLList
+    GraphQLList,
+    GraphQLInt
 } from 'graphql';
 
 import { PersonType } from '../types/person';
@@ -8,9 +9,16 @@ import { getPeople } from '../../repositories/person';
 
 export const PeopleList = {
   type: new GraphQLList(PersonType),
-  resolve() {
-    var people = getPeople(0, 3);
-    return people;
+  args: {
+    count: {
+      type: GraphQLInt
+    },
+    offset: {
+      type: GraphQLInt
+    }
+  },
+  resolve(parent, {count = 10, offset = 0}) {
+    return getPeople(offset, count);
   },
   description: 'Retrieve the people list'
 };
